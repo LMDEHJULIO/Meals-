521abf922db4b940ad4c56446e19fa6a8d343798
juliodevelops <juliodevelops@gmail.com>
2017-10-14 13:09:15 +0000

    mapping Redux object to array for meals

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,19 @@ class App extends Component {
 }
 
 function mapStateToProps (calendar) {
+  
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
   return {
-    name: 'Julio'
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+          
+          return meals
+      }, {})
+    }))
   }
 }
 
